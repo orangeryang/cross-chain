b9e1c130783c90e33002cb8c561eae7748e493ad
steif <you@example.com>
2023-12-10 20:59:23 +0800

    debug

--- ethereum/ether.sol
+++ ethereum/ether.sol
@@ -1,35 +1,38 @@
 // SPDX-License-Identifier: MIT
-// 0x081dc2733d92a18d297CD770590FACE57B577a2C
+// 0x63feb1704da0d3d537796b0c630e6ed340faf958
 
 pragma solidity ^0.8.4;
 
 contract Test {
+    event MessageHash(bytes32 msgHash, uint256 nonce);
+
     constructor() {}
 
     function test() public payable returns (bytes32, uint256) {
-        uint256[] memory data = new uint256[](6);
+        uint256[] memory data = new uint256[](5);
         uint256 seed = 74972505546522576923818356636106108867838777210876644873069541021231591033714;
         uint128 low = uint128(seed);
         uint128 high = uint128(seed >> 128);
 
+        data[0] = 1;
+        data[1] = uint256(low);
+        data[2] = uint256(high);
+        data[3] = uint256(uint160(address(this)));
         data[
-            0
-        ] = 0x018a77a46f9e8d742aa9642bb5e3fb510eda6b8ebc1ac36f6109e1c60d6f838a;
-        data[1] = 1;
-        data[2] = low;
-        data[3] = high;
-        data[4] = uint256(uint160(address(this)));
-        data[
-            5
+            4
         ] = 0x00d0c2b4aB3d70769B34DFfA5Ee5eee11a54cD1D6dA4E89bf97AFa8bA35770f1;
 
-        return
-            Starknet(0xde29d060D45901Fb19ED6C6e959EB22d8626708e)
-                .sendMessageToL2{value: msg.value}(
-                0x018a77a46f9e8d742aa9642bb5e3fb510eda6b8ebc1ac36f6109e1c60d6f838a,
-                0x01962ff092d95f8556c80858905718710284206123c65fbd6e5ca7e076b21c15,
-                data
-            );
+        (bytes32 msgHash, uint256 nonce) = Starknet(
+            0xde29d060D45901Fb19ED6C6e959EB22d8626708e
+        ).sendMessageToL2{value: msg.value}(
+            0x076130542bcc89b85d6d0570ce1a16fb4a8a4b40cdcc53cad9a69d69f94ee88b,
+            0x0017c00f03de8b5bd58d2016b59d251c13056b989171c5852949903bc043bc27,
+            data
+        );
+
+        emit MessageHash(msgHash, nonce);
+
+        return (msgHash, nonce);
     }
 }
 
--- starknet/src/lib.cairo
+++ starknet/src/lib.cairo
@@ -6,25 +6,64 @@ mod gotit {
 
     #[storage]
     struct Storage {
-        seed: LegacyMap::<u128, u256>,
+        seed: LegacyMap::<u256, u256>,
+        sam: LegacyMap::<u128, u128>
+    }
+
+    #[event]
+    #[derive(Drop, starknet::Event)]
+    enum Event {
+        BeCalled: BeCalled,
+        Sampled: Sampled
+    }
+
+    #[derive(Drop, starknet::Event)]
+    struct BeCalled {
+        #[key]
+        from_address: felt252,
+        #[key]
+        id: u256,
+        seed: u256,
+        eth_account: felt252,
+        starknet_account: ContractAddress
+    }
+
+    #[derive(Drop, starknet::Event)]
+    struct Sampled {
+        #[key]
+        from_address: felt252,
+        #[key]
+        id: u128,
+        seed: u128
     }
 
     #[l1_handler]
     fn get(
         ref self: ContractState,
         from_address: felt252,
-        id: u128,
+        id: u256,
         seed: u256,
         eth_account: felt252,
         starknet_account: ContractAddress
     ) -> felt252 {
+        self.emit(BeCalled { from_address, id, seed, eth_account, starknet_account });
+
         self.seed.write(id, seed);
 
         from_address
     }
 
+    #[l1_handler]
+    fn sample(ref self: ContractState, from_address: felt252, id: u128, seed: u128,) -> felt252 {
+        self.emit(Sampled { from_address, id, seed });
+
+        self.sam.write(id, seed);
+
+        from_address
+    }
+
     #[external(v0)]
-    fn get_seed(self: @ContractState, id: u128) -> u256 {
+    fn get_seed(self: @ContractState, id: u256) -> u256 {
         self.seed.read(id)
     }
 }
